5ec9028aba0c5847f80d6a5c23622b9ed2b402e8
ardfordev <ardfordev@gmail.com>
2024-03-09 12:42:13 +0700

    first commit

---
+++ app/contacts/page.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+import ContactTable from "@/components/ContactTable";
+import Search from "@/components/Search";
+import Pagination from "@/components/pagination";
+import { TableSkeleton } from "@/components/Skeleton";
+import { CreateButton } from "@/components/Buttons";
+import { getContactPages } from "@/lib/data";
+
+const Contact = async ({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await getContactPages(query);
+
+  return (
+    <div className="max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-between gap-1 mb-5">
+        <Search />
+        <CreateButton />
+      </div>
+      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+        <ContactTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="flex justify-center mt-4">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
